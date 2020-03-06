7864b344fab632ba69c83982b1fa0110956fd781
jakubenglicky <jakubenglicky@gmail.com>
2020-03-06 14:21:56 +0100

    Úprava deploy scriptu staging.sh => beta.sh
    - přejmenování na beta.sh
    - doplnění custom volání, které budou per project
    - migrace zatím jen status, spouštění manuálně
    - chybí ukončení deploye na betu, pokud se pokazí composer atp.

---
+++ beta.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+echo ""
+echo "Proběhne sestavení BETA prostředí"
+echo $(date)
+echo "-------------------------------------------"
+echo ""
+
+echo "1. Stav před deployem"
+printf "beta -> "
+readlink beta
+printf "production -> "
+readlink production
+printf "production-previous -> "
+readlink production-previous
+echo ""
+
+cd repository
+
+echo "2. Příprava repozitáře"
+echo " - 2.1. vyčištění"
+git clean -xdf .
+echo ""
+
+echo " - 2.2. aktualizace kódu"
+git pull -p
+echo ""
+
+latestCommit=$(git log -n1 --format="%h")
+echo " - 2.3. poslední commit je $latestCommit"
+echo ""
+
+echo " - 2.4. export do ./releases/$latestCommit"
+git checkout-index -af --prefix=../releases/$latestCommit/
+echo $latestCommit | tee ../releases/$latestCommit/temp/commit > /dev/null
+echo ""
+
+cd ..
+
+echo "3. Sestavení release"
+
+cd ./releases/$latestCommit
+
+export COMPOSER_HOME=/home/pecka
+
+echo " - 3.1. Composer"
+make production-composer
+echo ""
+
+echo " - 3.2. Assets"
+make production-assets
+echo ""
+
+cd ../..
+
+echo " - 3.3. Symlinks"
+cp common/local.neon releases/$latestCommit/app/config/local.neon
+echo "app/config/local.neon"
+
+rm -rf releases/$latestCommit/log
+ln -sfn ../../common/log releases/$latestCommit/log
+echo "log/"
+
+rm -rf releases/$latestCommit/www/data/banners
+ln -sf ../../../../common/data/banners releases/$latestCommit/www/data/banners
+echo "www/data/banners"
+
+rm -rf releases/$latestCommit/www/data/images
+ln -sf ../../../../common/data/images releases/$latestCommit/www/data/images
+echo "www/data/images"
+
+
+cd ./releases/$latestCommit
+echo ""
+
+make production-links
+echo ""
+
+echo "4. Databázové migrace"
+echo " - 4.1. Stav migrací"
+php tools/db-migrate.php status
+
+echo ""
+
+echo " - 4.2. Provádění migrací"
+#php tools/db-migrate.php up
+
+echo ""
+cd ../..
+
+buildError=$?
+
+if [ $buildError -gt 0 ]; then
+
+	echo "5. Sestavení neproběhlo v pořádku, chybový kód je"
+	echo $buildError
+	echo ""
+
+else
+
+	echo "5. Beta se nastaví na poslední release - ./releases/$latestCommit"
+	ln -sfn releases/$latestCommit beta
+
+  echo ""
+	cd ./beta
+	make deploy-notify-beta
+	cd ..
+	echo ""
+
+fi;
+
+echo "6. Stav po deployi do BETA prostředí"
+printf "beta -> "
+readlink beta
+printf "production -> "
+readlink production
+printf "production-previous -> "
+readlink production-previous
+echo ""
+
+echo "7. Odeberou se stará sestavení"
+betaRelease=$(readlink beta | cut -d / -f 2)
+productionRelease=$(readlink production | cut -d / -f 2)
+productionPreviousRelease=$(readlink production-previous | cut -d / -f 2)
+
+cd releases
+rm -rf `ls -t | grep -v $betaRelease | grep -v $productionRelease | grep -v $productionPreviousRelease | tail -n +2`
+ls -lt .
+
+echo ""
+
+echo "8. Využití disku"
+df .
+echo ""
+
+echo "9. Konec aktualizace"
+echo $(date)
